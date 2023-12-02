37dd5be480bba411431c1c3d3867c09ab47135f6
chriscotimms <chriscotimms@gmail.com>
2023-12-02 12:43:12 +0000

    working chat message history, passes into openai.js, then result is added to history.

--- openai.js
+++ openai.js
@@ -2,27 +2,29 @@
 //retrieves keys
 require("dotenv").config();
 
-//retrive openai api
+//retrieve openai api
 const OpenAI = require("openai").OpenAI;
 const openai = new OpenAI();
 let ChatGptMsg = "";
 
-async function main(userMsg) {
+async function main(msgHistory) {
+  console.log(msgHistory);
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     max_tokens: 200,
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant.",
-      },
-      {
-        role: "user",
-        content: userMsg,
-      },
-    ],
+    messages: msgHistory,
+    //msgHistory replaces content below
+    // [
+    //   {
+    //     role: "system",
+    //     content: "You are a helpful assistant.",
+    //   },
+    //   {
+    //     role: "user",
+    //     content: userMsg, (deleted variable)
+    //   },
+    // ],
   });
-  //console.log("completion.choices:", completion.choices);
   console.log("bot answer:", completion.choices[0].message.content);
   ChatGptMsg = completion.choices[0].message.content;
   return ChatGptMsg;
@@ -33,6 +35,5 @@ const debug = () => {
   return "openai.js running";
 };
 
-// Adding the code below to allow importing
-// the functions in other files
+// export variables to other files
 module.exports = { debug, main, ChatGptMsg };
